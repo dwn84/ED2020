c6484411030bebeb7e3f8b75413110741d89c2f5
dwn84 <dwn84@hotmail.com>
2020-10-27 07:47:50 -0500

    BFS

--- Grafos/src/grafos/GrafoN.java
+++ Grafos/src/grafos/GrafoN.java
@@ -12,26 +12,96 @@ import java.util.LinkedList;
  * @author samaniw
  */
 public class GrafoN {
-    
+
     private boolean MatrizAdyacencia[][];
     private LinkedList<Integer> ListaAdyacencia[];
 
     /**
      * Grafo estático donde se predetermina la cantidad de nodos
+     *
      * @param n representa la cantidad total de nodos en la estructura
      */
     public GrafoN(int n) {
         MatrizAdyacencia = new boolean[n][n];
         ListaAdyacencia = new LinkedList[n];
-        
+
         for (int i = 0; i < n; i++) {
-            ListaAdyacencia[i]= new LinkedList<>();
+            ListaAdyacencia[i] = new LinkedList<>();
         }
     }
-    
-    public void agregarArco(int origen, int destino){
+
+    public void agregarArco(int origen, int destino) {
         MatrizAdyacencia[origen][destino] = true;
         ListaAdyacencia[origen].add(destino);
     }
-    
+
+    public void eliminarArco(int origen, int destino) {
+        MatrizAdyacencia[origen][destino] = false;
+        ListaAdyacencia[origen].remove(destino);
+    }
+
+    public String mostrarMatriz() {
+        String tabla = "";
+        int tam = MatrizAdyacencia.length;
+        for (int i = 0; i < tam; i++) {
+            for (int j = 0; j < tam; j++) {
+                tabla += MatrizAdyacencia[i][j] ? 1 : 0;
+            }
+            tabla += "\n";
+        }
+        return tabla;
+    }
+
+    public String mostrarListaAdyacencia() {
+        String lista = "";
+        int tam = ListaAdyacencia.length;
+        for (int i = 0; i < tam; i++) {
+            lista += i + ": ";
+            for (Integer nodo : ListaAdyacencia[i]) {
+                lista += nodo + " ";
+            }
+            lista += "\n";
+        }
+        return lista;
+    }
+
+    public String mostrarArcos() {
+        String arcos = "";
+        int tam = MatrizAdyacencia.length;
+        for (int i = 0; i < tam; i++) {
+            for (int j = 0; j < tam; j++) {
+                if (MatrizAdyacencia[i][j]) {
+                    arcos += i + " " + j + "\n";
+                }
+            }
+        }
+        return arcos;
+    }
+
+    /**
+     * BFS
+     *
+     * @param origen Punto inicial del recorrido
+     */
+    public void recorridoAnchura(int origen) {
+        boolean visitados[] = new boolean[MatrizAdyacencia.length];
+        LinkedList<Integer> cola = new LinkedList<>();
+
+        cola.add(origen);
+        visitados[origen] = true;
+
+        while (!cola.isEmpty()) {
+            origen = cola.remove();
+            System.out.print(origen + " ");
+
+            for (Integer vecino : ListaAdyacencia[origen]) {
+                if (!visitados[vecino]) {
+                    visitados[vecino] = true;
+                    cola.add(vecino);
+                }
+            }
+        }
+
+    }
+
 }
--- Grafos/src/grafos/Grafos.java
+++ Grafos/src/grafos/Grafos.java
@@ -16,6 +16,19 @@ public class Grafos {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        GrafoN miniGrafo = new GrafoN(4);
+        miniGrafo.agregarArco(0, 2);
+        miniGrafo.agregarArco(0, 3);
+        miniGrafo.agregarArco(2, 3);
+        miniGrafo.agregarArco(1, 2);
+        System.out.println("Matriz de adyacencia");
+        System.out.println(miniGrafo.mostrarMatriz());
+        System.out.println("Lista de adyacencia");
+        System.out.println(miniGrafo.mostrarListaAdyacencia());
+        System.out.println("Lista de arcos");
+        System.out.println(miniGrafo.mostrarArcos());
+        System.out.println("Recorrido en anchura");
+        miniGrafo.recorridoAnchura(0);
     }
     
 }
